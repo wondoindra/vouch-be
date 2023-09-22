81bfc098fa1c45dd88ff8c2fb2c61de75541df5b
Wondo Indra <wondoindra28@gmail.com>
2023-09-22 19:48:03 +0700

    init socket connection

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require('express')
 const http = require('http')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+const { Server } = require("socket.io")
+
 
 const PORT = process.env.PORT || 8080;
 
 const app = express()
-const server = http.createServer(app)
-
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/', (req, res) => {
+  console.log('hello')
   res.send('<h1>Hello world</h1>')
+})
+
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: { origin: true }
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
 })
 
 server.listen(PORT, () => {
